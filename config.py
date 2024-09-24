b318ead5fc36d35d561b3c535fabaa188bef202e
Tanya097 <ritukumari8691@gmail.com>
2024-09-24 11:52:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Gunjan890/BABYMUSIC",
+    "https://github.com/Tanya097/TANYAMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+TkkVwWjSqWc5MDc1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+TkkVwWjSqWc5MDc1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Bihar_police_bihar_daroga_bss")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Bihar_police_bihar_daroga_bss")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
